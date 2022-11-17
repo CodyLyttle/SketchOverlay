ea437e98d405ae71f5f6536f0e3e0d8e8e44334c
Cody Lyttle <codylyttle123@gmail.com>
2022-11-17 14:06:45 +1300

    Remove support for all non-windows platforms.

--- SketchOverlay/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace SketchOverlay;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
