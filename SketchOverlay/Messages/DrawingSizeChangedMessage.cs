2805fca4c0f3779d6a8b078f073fc96d5bfd3837
Cody Lyttle <codylyttle123@gmail.com>
2022-11-23 23:35:58 +1300

    Add TODO.

--- SketchOverlay/Messages/DrawingSizeChangedMessage.cs
+++ SketchOverlay/Messages/DrawingSizeChangedMessage.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.Messaging.Messages;
 
 namespace SketchOverlay.Messages;
+
+// TODO: Look at using PropertyChangedMessage instead.
+// See: https://github.com/CommunityToolkit/dotnet/tree/main/src/CommunityToolkit.Mvvm/Messaging/Messages
 public class DrawingSizeChangedMessage : ValueChangedMessage<float>
 {
     public DrawingSizeChangedMessage(float value) : base(value)
