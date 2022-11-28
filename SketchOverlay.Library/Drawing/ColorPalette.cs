b2b9025128cf87da43f1813f550ad1f5a46bee6c
Cody Lyttle <codylyttle123@gmail.com>
2022-11-28 16:32:14 +1300

    Add ColorPalette.

---
+++ SketchOverlay.Library/Drawing/ColorPalette.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+
+namespace SketchOverlay.Library.Drawing;
+
+public class ColorPalette : IColorPalette
+{
+    private static readonly Color[] DarkColors =
+    {
+        Color.Black,
+        Color.DarkRed,
+        Color.Orange,
+        Color.DarkGreen,
+        Color.DarkBlue,
+        Color.DarkViolet
+    };
+
+    private static readonly Color[] MediumColors =
+    {
+        Color.Gray,
+        Color.Red,
+        Color.Gold,
+        Color.ForestGreen,
+        Color.Blue,
+        Color.Violet
+    };
+
+    private static readonly Color[] LightColors =
+    {
+        Color.White,
+        Color.OrangeRed,
+        Color.Yellow,
+        Color.LawnGreen,
+        Color.DeepSkyBlue,
+        Color.PaleVioletRed
+    };
+
+    public ColorPalette()
+    {
+        PrimaryColor = DefaultPrimaryColor;
+        SecondaryColor = DefaultSecondaryColor;
+    }
+
+    public ColorPalette(Color primaryColor, Color secondaryColor)
+    {
+        PrimaryColor = primaryColor;
+        SecondaryColor = secondaryColor;
+    }
+
+    public IEnumerable<Color> Colors => DarkColors
+        .Concat(MediumColors)
+        .Concat(LightColors);
+
+    public Color DefaultPrimaryColor => DarkColors[0];
+    
+    public Color DefaultSecondaryColor => LightColors[0];
+    
+    public Color PrimaryColor { get; set; }
+    
+    public Color SecondaryColor { get; set; }
+}
---
+++ SketchOverlay.Library/Drawing/IColorPalette.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace SketchOverlay.Library.Drawing;
+
+public interface IColorPalette
+{
+    public IEnumerable<Color> Colors { get; }
+    public Color DefaultPrimaryColor { get; }
+    public Color DefaultSecondaryColor { get; }
+    public Color PrimaryColor { get; set; }
+    public Color SecondaryColor { get; set; }
+}
