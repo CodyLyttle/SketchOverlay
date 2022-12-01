0864279cee38b06e4a2c9ed172e057e5d9965d39
Cody Lyttle <codylyttle123@gmail.com>
2022-12-02 12:06:45 +1300

    Add views.

--- SketchOverlay.Wpf/Views/DrawingToolWindow.xaml.cs
+++ SketchOverlay.Wpf/Views/DrawingToolWindow.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SketchOverlay.Wpf
+namespace SketchOverlay.Wpf.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DrawingToolWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DrawingToolWindow : UserControl
     {
-        public MainWindow()
+        public DrawingToolWindow()
         {
             InitializeComponent();
         }
---
+++ SketchOverlay.Wpf/Views/OverlayWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SketchOverlay.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for OverlayWindow.xaml
+    /// </summary>
+    public partial class OverlayWindow : Window
+    {
+        public OverlayWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
