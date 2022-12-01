f08550e5d46f1ee25256921bfb4626917ff1fe6e
Cody Lyttle <codylyttle123@gmail.com>
2022-12-02 12:16:27 +1300

    Add view models.

---
+++ SketchOverlay.Wpf/ViewModels/WpfDrawingToolWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media;
+using CommunityToolkit.Mvvm.Messaging;
+using SketchOverlay.Library.Drawing;
+using SketchOverlay.Library.ViewModels;
+
+namespace SketchOverlay.Wpf.ViewModels;
+
+internal class WpfDrawingToolWindowViewModel : DrawingToolWindowViewModel<Drawing, ImageSource, Color>
+{
+    public WpfDrawingToolWindowViewModel(ICanvasProperties<Color> canvasProps,
+        IColorPalette<Color> drawingColors,
+        IDrawingToolCollection<Drawing, ImageSource, Color> drawingTools,
+        IMessenger messenger) : base(canvasProps, drawingColors, drawingTools, messenger)
+    {
+    }
+}
---
+++ SketchOverlay.Wpf/ViewModels/WpfOverlayWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+using CommunityToolkit.Mvvm.Messaging;
+using SketchOverlay.Library.Drawing;
+using SketchOverlay.Library.ViewModels;
+
+namespace SketchOverlay.Wpf.ViewModels;
+
+internal class WpfOverlayWindowViewModel : OverlayWindowViewModel<Drawing, DrawingGroup, ImageSource, Color>
+{
+    public WpfOverlayWindowViewModel(ICanvasManager<DrawingGroup> canvasManager, IMessenger messenger) 
+        : base(canvasManager, messenger)
+    {
+    }
+}
