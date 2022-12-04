123a80ed96a63977e2a805d1a372fa498ab8ceb7
Cody Lyttle <codylyttle123@gmail.com>
2022-12-04 17:02:06 +1300

    Add wpf thickness converter.

---
+++ SketchOverlay.Wpf.Tests/LibraryThicknessToWindowsThicknessConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Windows;
+using SketchOverlay.Library.Models;
+using SketchOverlay.Wpf.BindingConverters;
+
+namespace SketchOverlay.Wpf.Tests;
+
+public class LibraryThicknessToWindowsThicknessConverterTests
+{
+    private readonly LibraryThicknessToWindowsThicknessConverter _sut;
+
+    public LibraryThicknessToWindowsThicknessConverterTests()
+    {
+        _sut = new LibraryThicknessToWindowsThicknessConverter();
+    }
+
+    [Fact]
+    public void Convert_ConvertsLibraryThicknessToEquivalentWindowsThickness()
+    {
+        // Arrange
+        LibraryThickness libraryThickness = new(-22, -11, 11, 22);
+
+        // Act
+        var windowsThickness = (Thickness)_sut.Convert(
+            libraryThickness, null!, null!, null!);
+
+        // Assert
+        AssertEquivalentThickness(libraryThickness, windowsThickness);
+    }
+
+    [Fact]
+    public void ConvertBack_ConvertsWindowsThicknessToEquivalentLibraryThickness()
+    {
+        // Arrange
+        Thickness windowsThickness = new(-22, -11, 11, 22);
+
+        // Act
+        var libraryThickness = (LibraryThickness)_sut.ConvertBack(
+            windowsThickness, null!, null!, null!);
+
+        // Assert
+        AssertEquivalentThickness(libraryThickness, windowsThickness);
+    }
+
+    private static void AssertEquivalentThickness(LibraryThickness libraryThickness, Thickness thickness)
+    {
+        Assert.Equal(libraryThickness.Left, thickness.Left);
+        Assert.Equal(libraryThickness.Top, thickness.Top);
+        Assert.Equal(libraryThickness.Right, thickness.Right);
+        Assert.Equal(libraryThickness.Bottom, thickness.Bottom);
+    }
+}
---
+++ SketchOverlay.Wpf/BindingConverters/LibraryThicknessToWindowsThicknessConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using SketchOverlay.Library;
+using SketchOverlay.Library.Models;
+
+namespace SketchOverlay.Wpf.BindingConverters;
+
+internal class LibraryThicknessToWindowsThicknessConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not LibraryThickness libraryThickness)
+            throw new ValueConverterTypeException<LibraryThickness>(value);
+
+        return new Thickness(
+            libraryThickness.Left, 
+            libraryThickness.Top, 
+            libraryThickness.Right,
+            libraryThickness.Bottom);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not Thickness windowsThickness)
+            throw new ValueConverterTypeException<Thickness>(value);
+
+        return new LibraryThickness(
+            windowsThickness.Left,
+            windowsThickness.Top,
+            windowsThickness.Right,
+            windowsThickness.Bottom);
+    }
+}
