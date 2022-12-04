60c47ac3e9aa1d99f10e7d7b2f70235c44d28006
Cody Lyttle <codylyttle123@gmail.com>
2022-12-04 16:07:05 +1300

    Add inverted boolean converter.

---
+++ SketchOverlay.Wpf.Tests/InvertedBooleanConverterTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using SketchOverlay.Wpf.BindingConverters;
+
+namespace SketchOverlay.Wpf.Tests;
+
+public class InvertedBooleanConverterTests
+{
+    private readonly InvertedBooleanConverter _sut;
+
+    public InvertedBooleanConverterTests()
+    {
+        _sut = new InvertedBooleanConverter();
+    }
+
+    [Fact]
+    public void Convert_InvertsValue()
+    {
+        Assert.False((bool)_sut.Convert(true, typeof(bool), null!, CultureInfo.CurrentCulture));
+        Assert.True((bool)_sut.Convert(false, typeof(bool), null!, CultureInfo.CurrentCulture));
+    }
+
+    [Fact]
+    public void ConvertBack_InvertsValue()
+    {
+        Assert.False((bool)_sut.Convert(true, typeof(bool), null!, CultureInfo.CurrentCulture));
+        Assert.True((bool)_sut.Convert(false, typeof(bool), null!, CultureInfo.CurrentCulture));
+    }
+}
---
+++ SketchOverlay.Wpf/BindingConverters/InvertedBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using SketchOverlay.Library;
+
+namespace SketchOverlay.Wpf.BindingConverters;
+
+internal class InvertedBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+            throw new ValueConverterTypeException<bool>(value);
+
+        return !boolValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+            throw new ValueConverterTypeException<bool>(value);
+
+        return !boolValue;
+    }
+}
