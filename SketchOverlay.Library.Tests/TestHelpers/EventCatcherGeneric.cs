09c9edc176322882a48d50b98e188d7ff4c7064a
Cody Lyttle <codylyttle123@gmail.com>
2022-12-05 19:48:40 +1300

    Simplify testing events with EventCatcher class.

---
+++ SketchOverlay.Library.Tests/TestHelpers/EventCatcherGeneric.cs
@@ -0,0 +1,11 @@
+﻿namespace SketchOverlay.Library.Tests.TestHelpers;
+
+internal class EventCatcher<TArgs>
+{
+    public List<(object? s, TArgs e)> Received { get; } = new();
+
+    public void OnReceived(object? sender, TArgs args)
+    {
+        Received.Add((sender, args));
+    }
+}
