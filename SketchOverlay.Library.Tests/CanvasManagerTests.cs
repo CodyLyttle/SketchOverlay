afe9e6032ecab223f0476a7c3db8c5bac997d091
Cody Lyttle <codylyttle123@gmail.com>
2022-12-05 22:06:09 +1300

    Add FinishDrawing tests.

--- SketchOverlay.Library.Tests/CanvasManagerTests.cs
+++ SketchOverlay.Library.Tests/CanvasManagerTests.cs
@@ -18,7 +18,7 @@ public class CanvasManagerTests
     {
         _mockDrawStack = new Mock<IDrawingStack<object, object>>();
         _mockDrawStack.Setup(x => x.PushDrawing(It.IsAny<object>()))
-            .Callback(() =>  _mockStackCount++);
+            .Callback(() => _mockStackCount++);
         _mockDrawStack.Setup(x => x.Count)
             .Returns(() => _mockStackCount);
 
@@ -64,6 +64,16 @@ public class CanvasManagerTests
         _sut.DoDrawing(new PointF());
         ResetMockInvocations();
         Assert.True(_sut.IsDrawing);
+    }
+
+    private void SetupWithDrawings(int drawingCount)
+    {
+        Assert.True(drawingCount > 0);
+        for (var i = 0; i < drawingCount; i++)
+        {
+            _sut.DoDrawing(new PointF());
+            _sut.FinishDrawing();
+        }
     }
 
     #region DoDrawing_WhileNotDrawing
@@ -223,6 +233,127 @@ public class CanvasManagerTests
 
         // Assert
         Assert.Empty(eventCatcher.Received);
+    }
+
+    #endregion
+
+    #region FinishDrawing
+
+    [Fact]
+    public void FinishDrawing_WhileNotDrawing_DoesNothing()
+    {
+        // Arrange
+        AssertNotDrawing();
+
+        // Act
+        _sut.FinishDrawing();
+
+        // Assert
+        AssertNotDrawing();
+        _mockCanvasProps.VerifyNoOtherCalls();
+        _mockDrawStack.VerifyNoOtherCalls();
+        _mockDrawingTool.VerifyNoOtherCalls();
+        _mockToolRetriever.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public void FinishDrawing_WhileDrawing_SetsIsDrawingToFalse()
+    {
+        // Arrange
+        SetupIsDrawing();
+
+        // Act
+        _sut.FinishDrawing();
+
+        // Assert
+        Assert.False(_sut.IsDrawing);
+    }
+
+    [Fact]
+    public void FinishDrawing_WhileDrawing_FinishesCurrentDrawing()
+    {
+        // Arrange
+        SetupIsDrawing();
+
+        // Act
+        _sut.FinishDrawing();
+
+        // Assert
+        _mockDrawingTool.Verify(x => x.FinishDrawing());
+        _mockDrawingTool.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public void FinishDrawing_WhileDrawing_InvokesRequestRedrawEvent()
+    {
+        // Arrange
+        SetupIsDrawing();
+        EventCatcher eventCatcher = new();
+        _sut.RequestRedraw += eventCatcher.OnReceived;
+
+        // Act
+        _sut.FinishDrawing();
+
+        // Assert
+        Assert.Single(eventCatcher.Received);
+    }
+
+    [Fact]
+    public void FinishDrawing_FirstDrawingInDrawStack_InvokesCanClearAndCanUndoEventsWithValueTrue()
+    {
+        // Arrange
+        AssertNotDrawing();
+        EventCatcher<bool> canClearCatcher = new();
+        EventCatcher<bool> canRedoCatcher = new();
+        EventCatcher<bool> canUndoCatcher = new();
+        _sut.CanClearChanged += canClearCatcher.OnReceived;
+        _sut.CanRedoChanged += canRedoCatcher.OnReceived;
+        _sut.CanUndoChanged += canUndoCatcher.OnReceived;
+
+        // Act
+        _sut.DoDrawing(new PointF());
+        _sut.FinishDrawing();
+
+        // Assert
+        Assert.Single(canClearCatcher.Received);
+        Assert.True(canClearCatcher.Received[0].e);
+        Assert.Single(canUndoCatcher.Received);
+        Assert.True(canUndoCatcher.Received[0].e);
+        Assert.Empty(canRedoCatcher.Received);
+    }
+
+    [Fact]
+    public void FinishDrawing_OnConsecutiveDrawing_DoesNotInvokeCanvasActionEvents()
+    {
+        // Arrange
+        SetupWithDrawings(1);
+        EventCatcher<bool> eventCatcher = CreateCanvasActionsEventCatcher();
+
+        // Act
+        _sut.DoDrawing(new PointF());
+        _sut.FinishDrawing();
+
+        // Assert
+        Assert.Empty(eventCatcher.Received);
+    }
+
+    [Fact]
+    public void FinishDrawing_WithCanRedoTrue_InvokesCanRedoWithValueFalse()
+    {
+        // Arrange
+        SetupWithDrawings(2);
+        _sut.Undo();
+        Assert.True(_sut.CanRedo);
+        EventCatcher<bool> eventCatcher = new();
+        _sut.CanRedoChanged += eventCatcher.OnReceived;
+
+        // Act
+        _sut.DoDrawing(new PointF());
+        _sut.FinishDrawing();
+
+        // Assert
+        Assert.Single(eventCatcher.Received);
+        Assert.False(eventCatcher.Received[0].e);
     }
 
     #endregion
