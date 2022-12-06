4d2aafc83d56dbae615b766ac294622878d2e8a5
Cody Lyttle <codylyttle123@gmail.com>
2022-12-06 16:57:48 +1300

    Add PropertyAssertions test helper.

---
+++ SketchOverlay.Library.Tests/TestHelpers/PropertyAssertions.cs
@@ -0,0 +1,17 @@
+﻿namespace SketchOverlay.Library.Tests.TestHelpers;
+
+internal static class PropertyAssertions
+{
+    public static void SetsInputValue<TValue>(object instance,
+        string propertyName, TValue inputValue)
+    {
+        // Arrange
+        instance.ThrowIfMatchingPropertyValue(propertyName, inputValue);
+
+        // Act
+        instance.SetPropertyValue(propertyName, inputValue);
+
+        // Act
+        Assert.Equal(inputValue, instance.GetPropertyValue<TValue>(propertyName));
+    }
+}
