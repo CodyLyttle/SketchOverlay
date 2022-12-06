edbeba6eb55b6384169dd182ffda6a74f98ef162
Cody Lyttle <codylyttle123@gmail.com>
2022-12-06 14:29:13 +1300

    Add base class getter to interface.

--- SketchOverlay.Library/Drawing/Tools/IDrawingTool.cs
+++ SketchOverlay.Library/Drawing/Tools/IDrawingTool.cs
@@ -5,6 +5,7 @@ namespace SketchOverlay.Library.Drawing.Tools;
 
 public interface IDrawingTool<out TDrawing, TColor>
 {
+    TDrawing CurrentDrawing { get; }
     TDrawing CreateDrawing(ICanvasProperties<TColor> canvasProps, PointF startPoint);
     void UpdateDrawing(PointF currentPoint);
     void FinishDrawing();
