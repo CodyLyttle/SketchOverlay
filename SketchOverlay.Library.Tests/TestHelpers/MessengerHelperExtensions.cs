39e2efe334477f6df2463e834bb155aab73e0695
Cody Lyttle <codylyttle123@gmail.com>
2022-12-07 14:25:13 +1300

    Generalise messenger extensions.

--- SketchOverlay.Library.Tests/TestHelpers/MessengerHelperExtensions.cs
+++ SketchOverlay.Library.Tests/TestHelpers/MessengerHelperExtensions.cs
@@ -2,11 +2,19 @@
 
 namespace SketchOverlay.Library.Tests.TestHelpers;
 
-internal static class MessengerAssertionExtensions
+internal static class MessengerHelperExtensions
 {
     public static void AssertIsRegistered<TMessage>(this IMessenger messenger, object sutInstance)
         where TMessage : class
     {
         MessengerAssertions.IsRegistered<TMessage>(messenger, sutInstance);
+    }
+
+    public static MessageInbox RegisterInbox<TMessage>(this IMessenger messenger)
+        where TMessage : class
+    {
+        MessageInbox inbox = new(messenger);
+        inbox.Register<TMessage>();
+        return inbox;
     }
 }
