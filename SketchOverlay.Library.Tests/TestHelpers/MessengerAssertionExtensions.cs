3309d87b7c9810da1c65d92d85f4bc182c723de3
Cody Lyttle <codylyttle123@gmail.com>
2022-12-07 14:02:50 +1300

    Add messenger assertion helper classes.

---
+++ SketchOverlay.Library.Tests/TestHelpers/MessengerAssertionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+namespace SketchOverlay.Library.Tests.TestHelpers;
+
+internal static class MessengerAssertionExtensions
+{
+    public static void AssertIsRegistered<TMessage>(this IMessenger messenger, object sutInstance)
+        where TMessage : class
+    {
+        MessengerAssertions.IsRegistered<TMessage>(messenger, sutInstance);
+    }
+}
---
+++ SketchOverlay.Library.Tests/TestHelpers/MessengerAssertions.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+namespace SketchOverlay.Library.Tests.TestHelpers;
+
+internal static class MessengerAssertions
+{
+    public static void IsRegistered<TMessage>(IMessenger messenger, object sutInstance)
+        where TMessage : class
+    {
+        Assert.True(messenger.IsRegistered<TMessage>(sutInstance));
+    }
+}
