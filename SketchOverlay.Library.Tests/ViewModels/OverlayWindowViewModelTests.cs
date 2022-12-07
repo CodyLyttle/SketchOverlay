dea3fb1b7c00b4037cda92dbc52f9f0ea4e9a878
Cody Lyttle <codylyttle123@gmail.com>
2022-12-07 14:03:07 +1300

    Add OverlayWindowViewModelTests.

---
+++ SketchOverlay.Library.Tests/ViewModels/OverlayWindowViewModelTests.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Moq;
+using SketchOverlay.Library.Drawing.Canvas;
+using SketchOverlay.Library.Messages;
+using SketchOverlay.Library.Tests.TestHelpers;
+using SUT = SketchOverlay.Library.ViewModels.OverlayWindowViewModel<object, object, object, object>;
+
+namespace SketchOverlay.Library.Tests.ViewModels;
+
+public class OverlayWindowViewModelTests
+{
+    #region Setups
+
+    private readonly SUT _sut;
+    private readonly IMessenger _messenger = WeakReferenceMessenger.Default;
+    private readonly Mock<ICanvasManager<object>> _mockCanvas;
+
+    public OverlayWindowViewModelTests()
+    {
+        _mockCanvas = new Mock<ICanvasManager<object>>();
+        _mockCanvas.SetupAllProperties();
+        _sut = new SUT(_mockCanvas.Object, _messenger);;
+    }
+
+    #endregion
+
+    #region Messenger
+
+    [Fact]
+    public void DrawingWindowPropertyChangedMessage_OnInstantiation_MessageIsRegistered()
+    {
+        _messenger.AssertIsRegistered<DrawingWindowPropertyChangedMessage>(_sut);
+    }
+
+    [Fact]
+    public void OverlayWindowCanvasActionMessage_OnInstantiation_MessageIsRegistered()
+    {
+        _messenger.AssertIsRegistered<OverlayWindowCanvasActionMessage>(_sut);
+    }
+
+    #endregion
+}
