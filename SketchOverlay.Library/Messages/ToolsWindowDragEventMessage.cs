6247051a1a09e13b616cbae6c6e1046097122807
Cody Lyttle <codylyttle123@gmail.com>
2022-12-07 16:15:12 +1300

    Fix param names.

--- SketchOverlay.Library/Messages/ToolsWindowDragEventMessage.cs
+++ SketchOverlay.Library/Messages/ToolsWindowDragEventMessage.cs
@@ -4,9 +4,9 @@ using SketchOverlay.Library.Models;
 
 namespace SketchOverlay.Library.Messages;
 
-public class ToolsWindowDragEventMessage : ValueChangedMessage<(DragAction, PointF position)>
+public class ToolsWindowDragEventMessage : ValueChangedMessage<(DragAction action, PointF position)>
 {
-    public ToolsWindowDragEventMessage(DragAction value, PointF position) : base((value, position))
+    public ToolsWindowDragEventMessage(DragAction action, PointF position) : base((action, position))
     {
     }
 }
