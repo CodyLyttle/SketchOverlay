46fa252b2ad5b926bb374e606e5f88ce4a51a306
Cody Lyttle <codylyttle123@gmail.com>
2022-12-08 13:05:33 +1300

    Add ValueConverterTypeExceptionTests.

---
+++ SketchOverlay.Library.Tests/ValueConverterTypeExceptionTests.cs
@@ -0,0 +1,15 @@
+﻿namespace SketchOverlay.Library.Tests;
+
+public class ValueConverterTypeExceptionTests
+{
+    [Fact]
+    public void ExceptionMessage_ContainsInformationAboutTypeMismatch()
+    {
+        // Arrange
+        const string expectedMessage = "Expected type \"String\", actual type \"Single\"";
+        ValueConverterTypeException<string> exception = new(123f);
+
+        // Assert
+        Assert.Equal(expectedMessage, exception.Message);
+    }
+}
